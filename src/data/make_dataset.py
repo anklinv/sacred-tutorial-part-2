685cd7f6863ba49f9cedc65fef843a398a9a1f26
Valentin Anklin <valentin@ankl.in>
2020-05-02 14:55:57 +0200

    Initialize

---
+++ src/data/make_dataset.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+from pathlib import Path
+import torchvision
+
+
+@click.command()
+@click.argument('output_filepath', type=click.Path())
+def main(output_filepath='data'):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+    torchvision.datasets.MNIST('data', train=True, download=True)
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
