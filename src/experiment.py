6fffc9d4975e62da8f2411cc4d670be73016fccb
Valentin Anklin <valentin@ankl.in>
2020-06-01 14:17:35 +0200

    Add solution experiment file

--- src/experiment.py
+++ src/experiment.py
@@ -1,22 +1,118 @@
 import getpass
 from sacred import Experiment
+from sacred.observers import MongoObserver
+import os
+from torch import optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import torchvision
+from models.simple_net import Net
 
 
 ex = Experiment("MNIST")
-# TODO: Add MongoDB
+ex.observers.append(MongoObserver('mongodb://mongo_user:mongo_password@localhost:27017/sacred?authSource=admin'))
 # TODO: Add SlackBot
 
 @ex.config
 def default():
     username = getpass.getuser()
-    # TODO: Add configuration
+    n_epochs = 10
+    batch_size_train = 64
+    batch_size_test = 1000
+    learning_rate = 0.01
+    momentum = 0.5
+    log_interval = 25
+    data_path = '../../data'
+    model_path = '../models'
+
+
+@ex.command
+def dataset(data_path):
+    torchvision.datasets.MNIST(data_path, train=True, download=True)
 
 
 @ex.automain
-def main():
+def main(n_epochs: int, batch_size_train: int, batch_size_test: int, learning_rate: float, momentum: float, log_interval: int, data_path: str, model_path: str):
     # TODO: Train and validate model
     # TODO: Add logging
     # TODO: Add metrics
     # TODO: Add a way to download the dataset
     # TODO: Improve model
-    pass
+
+
+    torch.backends.cudnn.enabled = False
+
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(data_path, train=True, download=False,
+                                transform=torchvision.transforms.Compose([
+                                    torchvision.transforms.ToTensor(),
+                                    torchvision.transforms.Normalize(
+                                        (0.1307,), (0.3081,))
+                                ])),
+        batch_size=batch_size_train, shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(data_path, train=False, download=False,
+                                transform=torchvision.transforms.Compose([
+                                    torchvision.transforms.ToTensor(),
+                                    torchvision.transforms.Normalize(
+                                        (0.1307,), (0.3081,))
+                                ])),
+        batch_size=batch_size_test, shuffle=True)
+
+
+    network = Net()
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+
+    def train(epoch):
+        network.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            step = epoch - 1 + batch_idx / len(train_loader)
+            optimizer.zero_grad()
+            output = network(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % log_interval == 0:
+                ex.log_scalar("train_loss", loss.item(), step)
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+                train_losses.append(loss.item())
+                train_counter.append(
+                    (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+                torch.save(network.state_dict(), os.path.join(model_path, 'model.pth'))
+                torch.save(optimizer.state_dict(), os.path.join(model_path, 'optimizer.pth'))
+
+
+    def test():
+        network.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = network(data)
+                test_loss += F.nll_loss(output, target, size_average=False).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(target.data.view_as(pred)).sum()
+        test_loss /= len(test_loader.dataset)
+        test_losses.append(test_loss)
+        ex.log_scalar("test_loss", float(test_loss))
+        ex.log_scalar("test_acc", float(100. * correct / len(test_loader.dataset)))
+        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+
+    test()
+    for epoch in range(1, n_epochs + 1):
+        train(epoch)
+        test()
+
